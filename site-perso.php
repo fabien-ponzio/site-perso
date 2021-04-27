5ba790239b3d31f9fab63874a06f900a6fb22a24
Fabien <fabien.ponzio@laplateforme.io>
2021-04-27 09:41:44 +0200

    final

--- site-perso.php
+++ site-perso.php
@@ -4,6 +4,15 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- META -->
+<meta description="Fabien Ponzio apprenti développeur web à la plateforme Marseille">
+<meta property="og:image" content="image/PhotoCV.jpg">
+<meta property="og:title" content="Fabien Ponzio Portfolio">
+<meta property="og:description" content="Apprenti Web Developper à La Plateforme_ Marseille.">
+<meta property="og:url" content="https://fabien-ponzio.students-laplateforme.io/">
+
+<!-- FAVICON -->
+<link rel="icon"  href='image/logobb.ico'/>
 <!-- FONT AWESOME CDN -->
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 <!-- SCRIPT JQUERY -->
@@ -17,7 +26,7 @@
 <!-- LINK GFONT  -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
-<title>Portfolio</title>
+<title>Portfolio - Fabien Ponzio</title>
 </head>
 
 <body>
@@ -34,9 +43,9 @@
         <span></span>
 
         <ul id="menu">
-          <a href="#"><li>Projets</li></a>
-          <a href="#"><li>A propos de moi</li></a>
-          <a href="#"><li>Contacts</li></a>
+          <a href="#about"><li>A propos de moi</li></a>
+          <a href="#test"><li>Projets</li></a>
+          <a href="#contact"><li>Contacts</li></a>
         </ul>
       </div>
 
@@ -63,28 +72,34 @@ Avec cette formation combiné à mon niveau d’anglais et mon aisance relationn
       <p>Mes Projets</p>
     <article class="box-slide">
         <section class="slide-show">
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
-            <a href="#" class="a-slide"> <img src="" class="img-slide" alt="logo"> </a>
+  
+            <a href="https://github.com/fabien-ponzio/voyage.git" class="a-slide"> <img src="image/voyage.PNG" class="img-slide" alt="voyage.git"> <span class="content">Site vitrine de voyage réalisé en HTML/CSS</span> </a>
+            <a href="https://github.com/fabien-ponzio/module-connexion.git" class="a-slide"> <img src="image/connexion.PNG" class="img-slide" alt="connexion.git"> <span class="content">Module de connexion réalisé en HTML/CSS, Backend en PHP</span> </a>
+            <a href="https://github.com/fabien-ponzio/blog.git" class="a-slide"> <img src="image/blog.PNG" class="img-slide" alt="blog.git"> <span class="content">Blog réalisé en HTML/CSS, Backend en PHP</span> </a>
+            <a href="https://github.com/fabien-ponzio/boutique.git" class="a-slide"> <img src="image/boutique.PNG" class="img-slide" alt="boutique.git"> <span class="content">Boutique en ligne réalisée en HTML/CSS, Backend en PHP/POO</span> </a>
+
         </section>
     </article>
     </div>
-
+    
     <div id="contact">
       <p>Contact</p>
+      <div class="container"> 
+      <img class="CV" src="image/PhotoCV.jpg" alt="Curriculum Vitae">
+      <div class="social">
+        <a href="https://www.linkedin.com/in/fabien-ponzio-030718150/">LinkedIn</a>
+        <a href="PONZIO_CV.pdf" download>CV</a>
+        <a href="https://github.com/fabien-ponzio">GitHub</a>
+      </div>
+      </div>
     </div>
-    <!-- <section>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam totam modi beatae, eos error aliquid animi facilis nisi magni accusamus tempore deserunt, accusantium iusto maiores laborum aperiam illum consequuntur nesciunt.</section> -->
+    <section>
 
+    </section>
 </main>
 
 <footer>
-
+  <p>copyright Fabien Ponzio 2021</p>
 </footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.1/chroma.min.js" integrity="sha512-RWI59o+PDXjPl3bakOf3k2ZbDtfvn/OU/ZKe6QmkE0V/ve7vYKEJe0RdkDueS+VkghBazP+1o4LKGON+pHUa5g==" crossorigin="anonymous"></script>
 <script type="text/javascript" src="portfolio.js"></script>
